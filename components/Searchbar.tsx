277b60cc438dd44e1dfd4d25dde15235e4fed2bc
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-02-07 12:38:28 +0100

    Config BrightData and setup scraper

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,17 +1,61 @@
 'use client';
+
+import { scrapeAndStoreProduct } from '@/lib/actions';
+import { FormEvent, useState } from 'react';
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+
+    const hostname = parsedURL.hostname;
+
+    // Check if hostname contains amazon.*
+    if (
+      hostname.includes('amazon.com') ||
+      hostname.includes('amazon.') ||
+      hostname.includes('amazon')
+    ) {
+      return true;
+    }
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+  return false;
+};
+
 const Searchbar = () => {
-  const handleSubmit = () => {
-    alert('hi');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = async (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    const isValidLink = isValidAmazonProductURL(searchQuery);
+
+    if (!isValidLink) return alert('Please provide a valid Amazon link');
+
+    try {
+      setIsLoading(true);
+
+      // scrape the product page
+      const product = await scrapeAndStoreProduct(searchQuery);
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
   };
+
   return (
     <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
       <input
         type='text'
         placeholder='Enter product link'
         className='searchbar-input'
+        onChange={(e) => setSearchQuery(e.target.value)}
+        value={searchQuery}
       />
-      <button type='submit' className='searchbar-btn'>
-        Search
+      <button type='submit' className='searchbar-btn' disabled={isLoading}>
+        {isLoading ? 'Searching...' : 'Search'}
       </button>
     </form>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: 'm.media-amazon.com', protocol: 'https', pathname: '**' },
+    ],
+  },
+};
 
 export default nextConfig;
